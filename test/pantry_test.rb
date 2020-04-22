9e456739ac56b675bb48e02c4ff423ac2ea9d5ab
Mad Hallé <mad.halle@icloud.com>
2020-04-22 09:24:15 -0600

    ingredient 2 fixed, all tests running and passing ingredient and pantry

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -8,6 +8,7 @@ class PantryTest < Minitest::Test
     @pantry = Pantry.new
 
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name:"Macaroni", unit:"oz", calories:200})
 
   end
 
