fc88c8ccd36d7a852a816822a6b753619d822bdc
Mad Hallé <mad.halle@icloud.com>
2020-04-22 09:10:44 -0600

    Ingredient class initialized

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -6,7 +6,7 @@ require './lib/ingredient'
 class IngredientTest < Minitest::Test
   def setup
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    @ingredient2 = Ingredient.new("Macaroni", "oz", 200)
+    # @ingredient2 = Ingredient.new("Macaroni", "oz", 200)
   end
 
   def test_it_exists
@@ -14,7 +14,7 @@ class IngredientTest < Minitest::Test
   end
 end
 # => #<Ingredient:0x007fe6041273d8...>
-# 
+#
 # pry(main)> ingredient1.name
 # # => "Cheese"
 #
