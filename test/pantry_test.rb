8feeadb883cc8674deba950e0b014a94adfddb3a
Mad Hallé <mad.halle@icloud.com>
2020-04-22 09:20:24 -0600

    restock ingredient test written

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -8,7 +8,7 @@ class PantryTest < Minitest::Test
     @pantry = Pantry.new
 
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    
+
   end
 
   def test_it_exists
@@ -22,6 +22,13 @@ class PantryTest < Minitest::Test
   def test_stock_check
     assert_equal 0, @pantry.stock_check(@ingredient1)
   end
+
+  def test_restock_ingredient
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+    assert_equal 15, @pantry.stock_check(@ingredient1)
+
+  end
 end
 # ```markdown
 # There are **4** Possible Points in Iteration 1:
@@ -30,19 +37,8 @@ end
 # 3. Pantry #check_stock
 # 4. Pantry #restock
 # ```
-# # => #<Pantry:0x007fd8858863b8...>
-#
 
-#
-# # => 0
-#
-# pry(main)> pantry.restock(ingredient1, 5)
-#
-# pry(main)> pantry.restock(ingredient1, 10)
-#
-# pry(main)> pantry.stock_check(ingredient1)
-# # => 15
-#
+
 # pry(main)> pantry.restock(ingredient2, 7)
 #
 # pry(main)> pantry.stock_check(ingredient2)
