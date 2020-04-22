15960db0d36cff0e2cf6d2292c8839121f6ac5fc
Mad Hallé <mad.halle@icloud.com>
2020-04-22 09:41:58 -0600

    add recipe test

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -7,6 +7,9 @@ require './lib/cook_book'
 class CookBookTest < Minitest::Test
   def setup
     @cookbook = CookBook.new
+
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
   end
 
   def test_it_exists
@@ -16,15 +19,10 @@ class CookBookTest < Minitest::Test
   def test_it_has_attributes
     assert_equal [], @cookbook.recipes
   end
-end
-# # => #<CookBook:0x00007faae6a42228 @recipes=[]>
-#
-# pry(main)> cookbook.add_recipe(recipe1)
-#
-# pry(main)> cookbook.add_recipe(recipe2)
-#
-# pry(main)> cookbook.recipes
-# # => [#<Recipe:0x00007faae69c9698...>, #<Recipe:0x00007faae692a110...>]
-# ```
 
-##
+  def test_add_recipe
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    assert_equal [@recipe1, @recipe2],@cookbook.recipes
+  end
+end
