8e6783c4ca7431c7353077e837ab7497ef073935
Mad Hallé <mad.halle@icloud.com>
2020-04-22 09:13:57 -0600

    attributes tested

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient
   attr_reader :name, :unit, :calories
-  def initialize(ingredient_info)
+  def initialize(ingredient_info )
     @name = ingredient_info[:name]
     @unit = ingredient_info[:unit]
     @calories = ingredient_info[:calories]
